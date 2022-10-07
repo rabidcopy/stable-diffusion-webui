ab412be105a9d35a8b187b05111935c8a51a1304
rabidcopy <rabidcopy@yahoo.com>
2022-10-07 01:06:31 -0500

    uncomment to load problematic models

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -123,6 +123,7 @@ def load_model_weights(model, checkpoint_file, sd_model_hash):
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
+    #sd = pl_sd 
 
     model.load_state_dict(sd, strict=False)
 
